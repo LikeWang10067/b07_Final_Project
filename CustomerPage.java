9d3e2cd145d8754a3212ffc227b9547f83a2a6f5
Felix Lee <felix.yy.lee@gmail.com>
2022-08-01 17:12:54 -0400

    Create java file for each page and interface redirection

---
+++ CustomerPage.java
@@ -0,0 +1,19 @@
+package com.example.myfirstapp;
+
+public class CustomerPage {
+    user User;
+    public CustomerPage(user User){
+        this.User = User;
+    }
+
+    public void listvenue(){}
+
+    public void listactivity(){}
+
+    public void join(){}
+
+    public void cancel(){}
+
+    public void logout(){}
+
+}
