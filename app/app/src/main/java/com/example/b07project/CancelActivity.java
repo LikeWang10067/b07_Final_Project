b9e11c18daaf6b879605cbbf868703b14f895e58
Felix Lee <felix.yy.lee@gmail.com>
2022-08-07 13:58:00 -0400

    Modify join and cancel activity

--- app/app/src/main/java/com/example/b07project/CancelActivity.java
+++ app/app/src/main/java/com/example/b07project/CancelActivity.java
@@ -30,10 +30,10 @@ public class CancelActivity extends AppCompatActivity implements View.OnClickLis
         event_name.setText(Event.getEventName());
 
         start = (TextView) findViewById(R.id.start);
-        start.setText(Event.getstart().toString());
+        start.setText(Event.getstart());
 
         end = (TextView) findViewById(R.id.end);
-        end.setText(Event.getend().toString());
+        end.setText(Event.getend());
 
         location = (TextView) findViewById(R.id.location);
         location.setText(Event.getVenue());
