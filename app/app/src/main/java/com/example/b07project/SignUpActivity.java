9061978f4cfd9a1b09e6af545ebbf7b4796b9e6e
Felix Lee <felix.yy.lee@gmail.com>
2022-08-08 17:22:05 -0400

    Modify SignUpActivity
    Solve sign up error

--- app/app/src/main/java/com/example/b07project/SignUpActivity.java
+++ app/app/src/main/java/com/example/b07project/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.example.b07project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,11 +42,13 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 //        int num_password = Integer.valueOf(password);
         Do.CheckSignUp(username, password, (user User) -> {
             if (User == null) {
+                Log.d("SignUp", "null");
                 this.username.setError("User already exist");
-                Intent intent = new Intent(this, MainActivity.class);
-                intent.putExtra("error_message", "User already exist");
-                startActivity(intent);
+//                Intent intent = new Intent(this, MainActivity.class);
+//                intent.putExtra("error_message", "User already exist");
+//                startActivity(intent);
             } else {
+                Log.d("SignUp", "SignUp");
                 Intent intent = new Intent(this, CustomerActivity.class);
                 intent.putExtra("user", User); //remember class user need to implement Serializable
                 startActivity(intent);
