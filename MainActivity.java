42506d6fac732d408d0f6efa2bee30fac0602084
kz4ever <64758307+kz4ever@users.noreply.github.com>
2022-08-08 14:43:04 -0400

    Change Password String

--- MainActivity.java
+++ MainActivity.java
@@ -1,39 +1,82 @@
-package com.example.myfirstapp;
+package com.example.b07project;
+
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnLogIn, btnSignUp;
+    private EditText username, password,password2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Writing to a realtime database
-        /*
-        DatabaseReference ref = FirebaseDatabase.getInstance("https://console.firebase.google.com/project/b07-final-project-91401/database/b07-final-project-91401-default-rtdb/data/~2F")
-                .getReference(); //update URL!!!
-        user student = new user("Runyu",1,false);
-        ref.child("User").child("Runyu").setValue(2);
-        */
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        password2 = (EditText) findViewById(R.id.password2);
+
+        btnLogIn = (Button) findViewById(R.id.btnLogIn);
+        btnLogIn.setOnClickListener(this);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(this);
     }
 
-    /**
-     * Called when the user taps the Send button
-     */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void LogIn() {
+        String username = this.username.getText().toString().trim();
+        String password2 = this.password2.getText().toString();
+        if (username.equals("") || password.equals("")) {
+            this.username.setError("Please input username");
+            this.password2.setError("Please input password");
+            return;
+        }else {
+            //int num_password = Integer.valueOf(password);
+            Do.CheckLogIn(username, password2, (user User) -> {
+                Log.d("User", String.valueOf(User == null));
+                if (User == null) {
+                    this.username.setError("User Incorrect or Password Incorrect");
+                    this.password2.setError("User Incorrect or Password Incorrect");
+                    //                Intent intent = new Intent(this, MainActivity.class);
+                    //                intent.putExtra("error_message", "User Incorrect or Password Incorrect");
+                    //                startActivity(intent);
+
+                } else if (User.get_admin() == 1) {
+                    Intent intent = new Intent(this, AdminActivity.class);
+                    intent.putExtra("user", User); //remember class user need to implement Serializable
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(this, CustomerActivity.class);
+                    intent.putExtra("user", User); //remember class user need to implement Serializable
+                    startActivity(intent);
+                }
+
+            });
+        }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogIn:
+                LogIn();
+                break;
+            case R.id.btnSignUp:
+                Intent intent = new Intent(this, SignUpActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
 }
